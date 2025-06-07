37267cbe9d0f1124f32b5d9a1755e820328e5357
Oleh Ridchenko <olehridchenko@gmail.com>
2025-06-07 21:11:46 +0300

    created all version of approaches to solve LCS, Cut-rod, F-sequence problems

--- src/main/LCS/LCSRecursive.java
+++ src/main/LCS/LCSRecursive.java
@@ -1,4 +1,15 @@
 package main.LCS;
 
 public class LCSRecursive {
+    public int getLongestCommonSubsequence(String first, String second, int i, int j) {
+        if (i == 0 || j == 0) return 0;
+
+        if (first.charAt(i - 1) == second.charAt(j - 1)) {
+            return 1 + getLongestCommonSubsequence(first, second, i - 1, j - 1);
+        } else {
+            return Math.max(getLongestCommonSubsequence(first, second, i - 1, j),
+                    getLongestCommonSubsequence(first, second, i, j - 1)
+            );
+        }
+    }
 }
